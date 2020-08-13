42b3c0b78c864c8773d92806aee8d6fa0be4b3f0
Fatima Amin <fatima.amin4official@gmail.com>
2020-08-13 15:22:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ class Breakfast {
 let bfast = new Breakfast('eggs', 'juice')
 bfast.food ;
 bfast.juice;
+
+class Lunch {
+  constructor(name , name2){
+    this.food = name ; 
+    this.drink = name2 ;
+  }
+}
