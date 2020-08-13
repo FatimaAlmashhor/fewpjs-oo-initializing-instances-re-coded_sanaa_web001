8f556ebdd6f14690790267046d32f8c5075392ed
Fatima Amin <fatima.amin4official@gmail.com>
2020-08-13 15:24:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ bfast.food ;
 bfast.juice;
 
 class Lunch {
-  constructor(name , name2){
-    this.food = name ; 
-    this.drink = name2 ;
+  constructor(name , name2 , name3){
+    this.salad; = name ;
+  
+    this.soup = name2 ;
+    this.drink =name3 ;
   }
 }
 
 let newlunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
-    newlunch.salad;
-  newlunch.soup;
-  newlunch.drink ; 
+newlunch.salad;
+newlunch.soup;
+newlunch.drink ; 
